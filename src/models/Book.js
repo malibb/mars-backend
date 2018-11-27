5d0bbbe3e1c894a67af260609973de2a32a26ffb
malistar98@gmail.com <malistar@gmail.com>
2018-11-26 20:49:19 -0600

    Home, User, Movie  Modification

--- src/models/Book.js
+++ src/models/Book.js
@@ -2,3 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 //Id Book, Id home, Id Usuario_Cliente, Fecha, soloImporte(ImporteDía, CostoTotal si acabamos el proyecto)
+
+const bookSchema = new Schema({
+    reservations	: {
+        dateStart	: Date,
+        dateEnd		: Date,
+        dateReserv	: { 
+            type: Date, 
+            default: Date.now,
+        },
+    }
+});
--- src/models/Home.js
+++ src/models/Home.js
@@ -1,4 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-//Id Home, Id User
+//*** Id Home, Id User
+const homeSchema = new Schema({
+
+		title		: String,
+		description	: String,
+		
+		/* etc */
+		photos		: {
+			title		: String,
+			url			: String,
+			dateCreted	: Date
+        }
+        
+        //Referencia a ***
+});

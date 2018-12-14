4fe73d6913c4779ac5f37533951a463ecddcb6b4
Angel RG <>
2018-12-13 21:29:36 -0600

    para unificar ramas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+
 import express from 'express';
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,7 +18,6 @@ ApolloServer.applyMiddleware({ app });
 app.get('/', (req, res) => {
     res.send({"message": "Bienvenido"});
 });
-
 app.listen( PORT, () => {
     console.log(`--- Servidor escuchando en el puerto ${PORT} ---`);
 });
